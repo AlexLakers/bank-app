dea7006d92ef98969756884190f4bb4bbb38dc1e
AlexLakers <alexey.nefedov199321@yandex.ru>
2026-02-23 21:46:00 +0300

    Init commit 1.Created multimodule project 'bank-app' 2.Added Microservices 4 subprojects( microservices) with base structure. 3.Added gitignore,base dependencies adn so on.

---
+++ bank-ui/src/main/java/com/alex/bank/ui/BankUiApplication.java
@@ -0,0 +1,13 @@
+package com.alex.bank.ui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankUiApplication.class, args);
+	}
+
+}
---
+++ bank-ui/src/test/java/com/alex/bank/ui/BankUiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alex.bank.ui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankUiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
