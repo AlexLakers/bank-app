ec7ddb2c7336f7dcd3039837a2725415855efc45
AlexLakers <alexey.nefedov199321@yandex.ru>
2026-02-26 22:19:49 +0300

    1.Added RestController-endpoints: 1.1 GET accounts/me - for geting auth account. 1.2 GET accounts?exclude.. - for getting all accounts. 1.3 Put /accounts - for update current account

---
+++ account-service/src/main/java/com/alex/bank/account/api/rest/controller/AccountController.java
@@ -0,0 +1,52 @@
+package com.alex.bank.account.api.rest.controller;
+
+import com.alex.bank.account.dto.AccountDto;
+import com.alex.bank.account.dto.AccountEditDto;
+import com.alex.bank.account.dto.MoneyOperationRequest;
+import com.alex.bank.account.dto.MoneyOperationResponse;
+import com.alex.bank.account.security.SecurityUtil;
+import com.alex.bank.account.service.AccountService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@RequestMapping("/accounts")
+@RequiredArgsConstructor
+@RestController
+public class AccountController {
+
+    public final AccountService accountService;
+
+    @GetMapping(value = "/me")
+    @PreAuthorize("hasRole('USER')")
+    public ResponseEntity<AccountDto> getAuthAccount(@AuthenticationPrincipal Jwt jwt) {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(accountService.getAccountByUsername(SecurityUtil.getCurrentUsernameFromJwtToken(jwt)));
+    }
+
+    @PreAuthorize("hasRole('USER') and hasAuthority('ACCOUNTS_WRITE')")
+    @GetMapping(params = "excludeCurrent")
+    public ResponseEntity<List<AccountDto>> getAccountsExcludeAuth(@AuthenticationPrincipal Jwt jwt) {
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(accountService.getAccountsExcludeOwner(SecurityUtil.getCurrentUsernameFromJwtToken(jwt)));
+    }
+
+    @PutMapping
+    @PreAuthorize("hasRole('USER') and hasAuthority('ACCOUNTS_WRITE')")
+    public ResponseEntity<AccountDto> updateUserDataAccount(@AuthenticationPrincipal Jwt jwt,
+                                                            @RequestBody AccountEditDto accountEditDto) {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(accountService.updateAccount(accountEditDto, SecurityUtil.getCurrentUsernameFromJwtToken(jwt)));
+    }
+
+
+}
