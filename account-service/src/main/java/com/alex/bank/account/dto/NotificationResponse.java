b394dc31185f14b0b0d0429e06bf3605c09fa9fe
AlexLakers <alexey.nefedov199321@yandex.ru>
2026-02-27 20:56:22 +0300

    1.Added dto for interaction with Notification-service 2.Added model for Outbox Transaction.

---
+++ account-service/src/main/java/com/alex/bank/account/dto/NotificationResponse.java
@@ -0,0 +1,6 @@
+package com.alex.bank.account.dto;
+
+import java.time.LocalDateTime;
+
+public record NotificationResponse(String notificationId, EventStatus status, LocalDateTime processedAt) {
+}
