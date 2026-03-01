2c62febae9e024d8e217d0efcea984352465ab52
AlexLakers <alexey.nefedov199321@yandex.ru>
2026-03-01 17:27:14 +0300

    1.I added PostgresTestconteinerConfig for Account-service and Cash-service(in future).

--- cash-service/src/test/java/com/alex/bank/cash/CashServiceApplicationTests.java
+++ cash-service/src/test/java/com/alex/bank/cash/CashServiceApplicationTests.java
@@ -1,9 +1,14 @@
 package com.alex.bank.cash;
 
+import com.alex.bank.cash.config.PostgresTestconteinerConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.context.ImportTestcontainers;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration")
+/*@Testcontainers*/
+/*@ImportTestcontainers({PostgresTestconteinerConfig.class})*/
 class CashServiceApplicationTests {
 
     @Test
