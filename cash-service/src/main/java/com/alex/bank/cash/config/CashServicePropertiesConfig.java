b18b2c2818ac3d6c8e7a115a95db24fb7c641210
AlexLakers <alexey.nefedov199321@yandex.ru>
2026-03-03 19:20:57 +0300

    Added own props for cash-service(urls).

---
+++ cash-service/src/main/java/com/alex/bank/cash/config/CashServicePropertiesConfig.java
@@ -0,0 +1,27 @@
+package com.alex.bank.cash.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@ConfigurationProperties(prefix = "cash")
+@Component
+@Getter
+@Setter
+public class CashServicePropertiesConfig {
+
+    private NotificationServiceProperties notificationService = new NotificationServiceProperties();
+    private AccountServiceProperties accountService = new AccountServiceProperties();
+
+    @Getter
+    @Setter
+    static class NotificationServiceProperties {
+        private String baseUrl="http://localhost:8086";
+    }
+    @Getter
+    @Setter
+    static class AccountServiceProperties {
+        private String baseUrl="http://localhost:8082";
+    }
+}
