f39c90946c1b11e0c6a31b0ccf26b87f6bf2c402
AlexLakers <alexey.nefedov199321@yandex.ru>
2026-03-07 12:56:07 +0300

    changed the tests for context loading using mock because the datasource test here is not necessary.

--- transfer-service/src/test/java/com/alex/bank/transfer/TransferServiceApplicationTests.java
+++ transfer-service/src/test/java/com/alex/bank/transfer/TransferServiceApplicationTests.java
@@ -1,13 +1,20 @@
 package com.alex.bank.transfer;
 
+import com.alex.bank.transfer.repository.OutboxRepository;
+import com.alex.bank.transfer.repository.TransferTransactionRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration")
 class TransferServiceApplicationTests {
+
+    @MockitoBean
+    private TransferTransactionRepository transferTransactionRepository;
+    @MockitoBean
+    private OutboxRepository outboxRepository;
 
     @Test
     void contextLoads() {
     }
-
 }
