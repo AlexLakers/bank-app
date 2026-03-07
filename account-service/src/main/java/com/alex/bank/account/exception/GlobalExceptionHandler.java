862d1641c9999d57b6d033611ac4d4d75a942b0e
AlexLakers <alexey.nefedov199321@yandex.ru>
2026-03-07 21:00:21 +0300

    1.added common exceptions in 'Account-service'

--- account-service/src/main/java/com/alex/bank/account/exception/GlobalExceptionHandler.java
+++ account-service/src/main/java/com/alex/bank/account/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.alex.bank.account.exception;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
+import com.alex.bank.common.exceptions.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
