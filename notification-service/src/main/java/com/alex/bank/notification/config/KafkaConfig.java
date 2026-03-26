1d0d5079b7648c1b910db3ba978c9a2f70ef4291
AlexLakers <alexey.nefedov199321@yandex.ru>
2026-03-26 20:44:07 +0300

    1.Added KafkaConfig for 'notification-service'-Consumer 1.1 CommonErrorHandler(Delegating) for different error cases. 1.2 RecordFilterStrategy for filter records using idempotency key. 1.3 I incapsulated all the beans in 'ConcurrentKafkaListenerContainerFactory' using multithreading.

--- notification-service/src/main/java/com/alex/bank/notification/config/KafkaConfig.java
+++ notification-service/src/main/java/com/alex/bank/notification/config/KafkaConfig.java
@@ -1,11 +1,18 @@
 package com.alex.bank.notification.config;
 
+import com.alex.bank.common.dto.notification.NotificationRequest;
+import com.alex.bank.notification.exception.NotificationDeserializationException;
+import com.alex.bank.notification.repository.EventIdempotenceRepository;
 import com.alex.bank.notification.service.DeadLetterQueueRecordRecoverer;
+import com.alex.bank.notification.service.IdempotencyKeyRecordFilterStrategy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.listener.CommonDelegatingErrorHandler;
 import org.springframework.kafka.listener.CommonErrorHandler;
 import org.springframework.kafka.listener.DefaultErrorHandler;
+import org.springframework.kafka.listener.adapter.RecordFilterStrategy;
 import org.springframework.util.backoff.FixedBackOff;
 
 import java.util.Map;
@@ -14,16 +21,36 @@ import java.util.Map;
 public class KafkaConfig {
     @Bean
     CommonErrorHandler commonErrorHandler(DeadLetterQueueRecordRecoverer deadLetterQueueRecordRecoverer) {
-        // Обработчик, который делает 10 попыток переполучения сообщения перед помещением в DLQ.
-        DefaultErrorHandler defaultErrorHandler = new DefaultErrorHandler(deadLetterQueueRecordRecoverer);
 
-        // Обработчик, который отправляет сообщение в DLQ сразу (без попыток).
-        DefaultErrorHandler deadLetterQueueErrorHandler = new DefaultErrorHandler(deadLetterQueueRecordRecoverer, new FixedBackOff(0, 0));
+        // 10 retries with delay 1000ms(general handler)
+       DefaultErrorHandler retryHandler=new DefaultErrorHandler(deadLetterQueueRecordRecoverer, new FixedBackOff(1000L, 9));
 
-        // Обработчик, который делегирует в deadLetterQueueErrorHandler в случае IllegalArgumentException
-        CommonDelegatingErrorHandler errorHandler = new CommonDelegatingErrorHandler(defaultErrorHandler);
-        errorHandler.setErrorHandlers(Map.of(IllegalArgumentException.class, deadLetterQueueErrorHandler));
+       //handler without retry for deserialization
+       DefaultErrorHandler noRetryHandler=new DefaultErrorHandler(deadLetterQueueRecordRecoverer, new FixedBackOff(0, 0));
 
-        return errorHandler;
+       CommonDelegatingErrorHandler commonDelegatingErrorHandler=new CommonDelegatingErrorHandler(retryHandler);
+       commonDelegatingErrorHandler.setErrorHandlers(Map.of(NotificationDeserializationException.class,noRetryHandler));
+        return commonDelegatingErrorHandler;
     }
+
+    @Bean
+    RecordFilterStrategy<String, NotificationRequest> idempotencyKeyRecordFilterStrategy(EventIdempotenceRepository eventIdempotenceRepository) {
+        return new IdempotencyKeyRecordFilterStrategy(eventIdempotenceRepository);
+    }
+
+    //base bean for consumer
+    @Bean
+    ConcurrentKafkaListenerContainerFactory<String, NotificationRequest> kafkaListenerContainerFactory(
+            ConsumerFactory<String, NotificationRequest> consumerFactory,
+            CommonErrorHandler commonErrorHandler,
+            RecordFilterStrategy<String, NotificationRequest> idempotencyKeyRecordFilterStrategy
+    ) {
+        ConcurrentKafkaListenerContainerFactory<String, NotificationRequest> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory); // for base settings of consumer
+        factory.setRecordFilterStrategy(idempotencyKeyRecordFilterStrategy); //for idempotency
+        factory.setCommonErrorHandler(commonErrorHandler); //for error handling
+        factory.setConcurrency(2);  //for two partitions
+        return factory;
+    }
+
 }
