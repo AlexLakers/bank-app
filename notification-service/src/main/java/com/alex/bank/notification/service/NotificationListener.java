c1108bd7e83f936742a752c3431349d0f5300af6
AlexLakers <alexey.nefedov199321@yandex.ru>
2026-04-03 17:35:20 +0300

    1.Added custom Spans for notification-service.

--- notification-service/src/main/java/com/alex/bank/notification/service/NotificationListener.java
+++ notification-service/src/main/java/com/alex/bank/notification/service/NotificationListener.java
@@ -3,6 +3,8 @@ package com.alex.bank.notification.service;
 import com.alex.bank.common.dto.notification.NotificationRequest;
 import com.alex.bank.notification.exception.NotificationDeserializationException;
 import com.alex.bank.notification.repository.EventIdempotenceRepository;
+import io.micrometer.tracing.Span;
+import io.micrometer.tracing.Tracer;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
@@ -18,17 +20,21 @@ import org.springframework.transaction.annotation.Transactional;
 public class NotificationListener {
 
     private final EventIdempotenceRepository eventIdempotenceRepository;
+    private final Tracer tracer;
 
-    @KafkaListener(topics = {"account-events", "cash-events","transfer-events"},
+    @KafkaListener(topics = {"account-events", "cash-events", "transfer-events"},
             containerFactory = "kafkaListenerContainerFactory", groupId = "notification-handlers")
     public void listen(ConsumerRecord<String, NotificationRequest> record, Acknowledgment acknowledgment) {
 
         NotificationRequest notification = record.value();
         log.info("Event [{} type:{} source:{} message:{} data:{}]",
                 notification.eventId(), notification.eventType(), notification.source(), notification.message(), notification.payload());
-        eventIdempotenceRepository.saveEvent(notification.eventId());
+        Span span = tracer.nextSpan().name("saveOutboxInDatabase").start();
+        try (Tracer.SpanInScope ws = tracer.withSpan(span)) {
+            eventIdempotenceRepository.saveEvent(notification.eventId());
+        } finally {
+            span.end();
+        }
         acknowledgment.acknowledge();
-
     }
-
 }
